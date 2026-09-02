500037ee31e02bd75f32938a06bf6164b5948f2a
henry <hudson.hm102826272@gmail.com>
2026-09-02 14:45:49 -0300

    Refactor product management templates and styles
    - Updated the product index page to enhance layout and styling, including a new header and filter options.
    - Improved the product form with better styling and structure, including responsive design for mobile devices.
    - Changed success message colors for better visibility and consistency.
    - Added new CSS styles for buttons, tables, and forms to improve user experience.
    - Updated JavaScript for real-time clock display in the header.
    - Modified HTML structure for better semantic meaning and accessibility.
    - Updated database schema to reflect recent changes.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 # Ponte de entrada do meu sistema
 from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from app.auth import get_usuario_opcional
 
 from app.controllers import auth_controller
+import os
 from app.controllers import usuario_controller
 from app.controllers import categoria_controller
 from app.controllers import produto_controller
@@ -17,6 +19,7 @@ app = FastAPI(title="Sistema de Ponto de venda")
 
 #Configurar a pasta para servir os arquivos estáticos (CSS, JS e IMG)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 
 #Configurar o jinja2 para renderizar os HTML
 templates = Jinja2Templates(directory="app/templates")
